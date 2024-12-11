06554a75112da06954914be42e5103e3914392c4
Jung Sung-Min <jsms91@hanmail.net>
2024-12-12 02:15:12 +0900

    p-create

---
+++ src/main/java/com/jung/todo/TodoApplication.java
@@ -0,0 +1,14 @@
+package com.jung.todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoApplication.class, args);
+		System.out.println("클러쉬 과제");
+	}
+
+}
